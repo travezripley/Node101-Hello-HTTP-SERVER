48fc9d5bfaffa415ed47554e12d425fb63a2414e
Travezripley <theripleys@gmail.com>
2019-03-16 11:34:08 -0700

    Add Initial Project Files

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// write your code here
+var http = require('http');
+
+http.createServer(function (req, res) {
+   
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end('Hello World');
+
+
+})
+.listen(8080, '127.0.0.1');
+console.log('Server running at https://127.0.0.1:8080/');
+
+
+/*http.createServer(app).listen(process.env.port || 3000, function() {
+    console.log('Express app started');
+})
+
+app.get('/', (req, res) => res.send("Hello World"))
+  
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))*/
